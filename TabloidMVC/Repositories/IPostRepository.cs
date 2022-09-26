5e6bd940e394fa0587402bbb4d001a25d0510e72
rjdny <therealglatrix@gmail.com>
2022-09-26 14:48:28 -0500

    Delete Posts

--- TabloidMVC/Repositories/IPostRepository.cs
+++ TabloidMVC/Repositories/IPostRepository.cs
@@ -6,6 +6,8 @@ namespace TabloidMVC.Repositories
     public interface IPostRepository
     {
         void Add(Post post);
+        void Delete(int postId);
+
         List<Post> GetAllPublishedPosts();
         Post GetPublishedPostById(int id);
         Post GetUserPostById(int id, int userProfileId);
